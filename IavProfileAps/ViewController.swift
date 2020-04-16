a0f2bcac9ddf07c2ea78b1923e80e446f361a465
iavtamvan <ade.fajr.ariav@gmail.com>
2020-04-16 19:55:26 +0700

    Initial Commit

---
+++ IavProfileAps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IavProfileAps
+//
+//  Created by iav on 16/04/20.
+//  Copyright © 2020 iav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

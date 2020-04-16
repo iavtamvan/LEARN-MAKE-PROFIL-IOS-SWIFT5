b59051e0bf6692cafa095342120c135f1dc72205
iavtamvan <ade.fajr.ariav@gmail.com>
2020-04-16 21:47:41 +0700

    FIRST COMMIT

--- IavProfileAps/ViewController.swift
+++ IavProfileAps/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func editProfileBtn(_ sender: Any) {
+        let alert = UIAlertController(title: "Apakah Anda berniat untuk mengubah profil?",
+               message: "Anda dapat memperbarui profil Anda sekarang juga.",
+               preferredStyle: .alert)
+        
+           alert.addAction(UIAlertAction(title: "Ya", style: .default, handler: { action in
+               print("Mengganti profile.")
+           }))
+        
+           alert.addAction(UIAlertAction(title: "Tidak", style: .cancel, handler: { action in
+               print("Membatalkan ubah profil.")
+           }))
+        
+           self.present(alert, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
